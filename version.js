3f1358fdf3c645520819a9ab3a8eed062a9d2566
hiroaki <kounavos@gmail.com>
2017-09-23 16:07:19 -0400

    (version 0.36)

--- version.js
+++ version.js
@@ -1,2 +1,2 @@
-if (undefined !== HiroFriends) HiroFriends.latestVersion = 0.35;
-if (undefined !== HiroFriends.updateManager) HiroFriends.updateManager.latestVersion = 0.35;
+if (undefined !== HiroFriends) HiroFriends.latestVersion = 0.36;
+if (undefined !== HiroFriends.updateManager) HiroFriends.updateManager.latestVersion = 0.36;
