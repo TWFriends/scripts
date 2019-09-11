5d7b9dc76f98b36297f311ac94a15a28578894fe
hiroaki <kounavos@gmail.com>
2019-09-11 13:03:08 -0400

    (version 0.60)

--- version.js
+++ version.js
@@ -1,2 +1,2 @@
-if (undefined !== HiroFriends) HiroFriends.latestVersion = 0.58;
-if (undefined !== HiroFriends.updateManager) HiroFriends.updateManager.latestVersion = 0.58;
+if (undefined !== HiroFriends) HiroFriends.latestVersion = 0.60;
+if (undefined !== HiroFriends.updateManager) HiroFriends.updateManager.latestVersion = 0.60;
