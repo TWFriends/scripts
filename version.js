aadd8eb16b98d3a8a6e838e9dc8734c31a62a900
TWFriends <kounavos@gmail.com>
2021-09-15 15:57:51 +0300

    (version 0.65)

--- version.js
+++ version.js
@@ -1,2 +1,2 @@
-if (undefined !== HiroFriends) HiroFriends.latestVersion = 0.62;
-if (undefined !== HiroFriends.updateManager) HiroFriends.updateManager.latestVersion = 0.62;
+if (undefined !== HiroFriends) HiroFriends.latestVersion = 0.65;
+if (undefined !== HiroFriends.updateManager) HiroFriends.updateManager.latestVersion = 0.65;
